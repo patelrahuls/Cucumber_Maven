559383840bda80abf3c43ec785b094302e0ee7e7
Rahul Patel <patelrahul1990@gmail.com>
2021-07-18 01:49:54 -0400

    First Cucumber Commit

---
+++ src/stefDefs/HomepageSteps.java
@@ -0,0 +1,29 @@
+package stefDefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomepageSteps {
+	
+	@Given("^user has landed on home screen$")
+    public void user_has_landed_on_home_screen(){
+        
+    }
+
+    @When("^when user do someting$")
+    public void when_user_do_someting(){
+        
+    }
+
+    @Then("^they should be able to see something$")
+    public void they_should_be_able_to_see_something(){
+       
+    }
+
+    @Then("^they should be anle to see something$")
+    public void they_should_be_anle_to_see_something(){
+        
+    }
+
+}
